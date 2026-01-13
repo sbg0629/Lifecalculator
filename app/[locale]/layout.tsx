c2960182ec9a32f1706e4733f81f093d28f22a2b
손봉균 <sbg0629@naver.com>
2026-01-13 12:14:47 +0900

    fix: speed lnsights

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from "next";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
@@ -57,6 +58,7 @@ export default async function LocaleLayout({
       </main>
       <Footer />
       <Analytics />
+      <SpeedInsights />
     </NextIntlClientProvider>
   );
 }
